9d89bf8b23ecf5802a4595a2e6d606efb3602cde
Yorick Peterse <yorickpeterse@gmail.com>
2018-07-19 01:34:30 +0200

    Remove stray cloudfront extension from config.rb

--- config.rb
+++ config.rb
@@ -57,12 +57,6 @@ end
 
 default_caching_policy max_age: 24 * 60 * 60
 
-activate :cloudfront do |cf|
-  cf.distribution_id = ENV['DISTRIBUTION_ID']
-  cf.access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  cf.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-end
-
 configure :development do
   activate :livereload
 end
