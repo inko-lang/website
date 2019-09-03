e45e8547d44fba5c34b292eb7b3c831acec78d83
Yorick Peterse <yorick@yorickpeterse.com>
2019-09-03 22:46:53 +0200

    Set up the Git remote for sponsors data
    [ci skip]

--- scripts/sponsors.sh
+++ scripts/sponsors.sh
@@ -4,52 +4,62 @@ function info() {
     echo -e "\\033[1m\\033[32m>>>\\033[0m\\033[0m ${1}"
 }
 
-if [[ -v CI ]]
-then
-    info 'Configuring SSH'
+function error() {
+    echo -e "\\033[1m\\033[31m!!!\\033[0m\\033[0m ${1}"
+    exit 1
+}
 
-    mkdir -p ~/.ssh
-    echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_ed25519.pub
-    echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
+function update() {
+    if [[ -v CI ]]
+    then
+        info 'Configuring SSH'
 
-    chmod 700 ~/.ssh
-    chmod 644 ~/.ssh/id_ed25519.pub
-    chmod 600 ~/.ssh/id_ed25519
+        mkdir -p ~/.ssh
+        echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_ed25519.pub
+        echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
 
-    eval "$(ssh-agent -s)"
+        chmod 700 ~/.ssh
+        chmod 644 ~/.ssh/id_ed25519.pub
+        chmod 600 ~/.ssh/id_ed25519
 
-    ssh-add ~/.ssh/id_ed25519
+        eval "$(ssh-agent -s)"
 
-    info 'Configuring Git'
+        ssh-add ~/.ssh/id_ed25519
 
-    git config --global user.email noreply@inko-lang.org
-    git config --global user.name 'Inko bot'
-fi
+        info 'Configuring Git'
 
-info 'Updating sponsors data'
+        git config --global user.email noreply@inko-lang.org
+        git config --global user.name 'Inko bot'
+        git remote add ssh git@gitlab.com:inko-lang/website.git >/dev/null 2>&1 || true
+    fi
 
-bundle exec rake sponsors:update sponsors:prune_logos
+    info 'Updating sponsors data'
 
-info 'Committing changes (if any)'
+    bundle exec rake sponsors:update sponsors:prune_logos
 
-git checkout --quiet master
-git add --all source/images/sponsors data/sponsors.yml
+    info 'Committing changes (if any)'
 
-if git commit -m 'Update sponsors data'
-then
-    for i in {1..3}
-    do
-        info 'Pushing to master'
+    git checkout --quiet master
+    git add --all source/images/sponsors data/sponsors.yml
 
-        git push origin master && break
+    if git commit -m 'Update sponsors data'
+    then
+        for i in {1..3}
+        do
+            info 'Pushing to master'
 
-        # A push might fail randomly, or because new commits are added. To
-        # handle the latter, we'll try to update the local clone before
-        # retrying.
-        git pull --rebase origin master
+            git push ssh master && return
 
-        info "Push attempt $i failed, retrying..."
-    done
-else
-    info 'Nothing to commit'
-fi
+            # A push might fail randomly, or because new commits are added. To
+            # handle the latter, we'll try to update the local clone before
+            # retrying.
+            git pull --rebase origin master >/dev/null 2>&1
+
+            info "Push attempt $i failed, retrying..."
+        done
+
+        error 'Failed to push the changes'
+    else
+        info 'Nothing to commit'
+    fi
+}
