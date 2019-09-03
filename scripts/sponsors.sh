a8d7410133ef1f562b6f43b4117f2fc3cf35f029
Yorick Peterse <yorick@yorickpeterse.com>
2019-09-03 23:02:51 +0200

    Set up known hosts for sponsors data

--- scripts/sponsors.sh
+++ scripts/sponsors.sh
@@ -15,10 +15,13 @@ function update() {
         info 'Configuring SSH'
 
         mkdir -p ~/.ssh
+
+        cp config/known_hosts ~/.ssh/known_hosts
         echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_ed25519.pub
         echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
 
         chmod 700 ~/.ssh
+        chmod 644 ~/.ssh/known_hosts
         chmod 644 ~/.ssh/id_ed25519.pub
         chmod 600 ~/.ssh/id_ed25519
 
