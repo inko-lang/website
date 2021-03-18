fdd06b1067a827e64b6e021500edb4f11f9d1d78
Yorick Peterse <yorick@yorickpeterse.com>
2021-03-18 18:39:28 +0100

    Only update sponsor logos when they change

--- lib/sponsors/image.rb
+++ lib/sponsors/image.rb
@@ -14,20 +14,32 @@ module Sponsors
     def download
       response = HTTP.get(@url)
 
-      if response.code != 200
+      unless response.code == 200
         raise "Failed to download #{@url}: #{response.reason}"
       end
 
       path = local_path_for(response)
+      remote_modified = Time.parse(response.headers['Last-Modified'])
+      log_output = `git log -1 --format=%ad #{path} 2>&1`.strip
+
+      update =
+        if $?.success?
+          Time.parse(log_output) < remote_modified
+        else
+          true
+        end
+
       body = response.body
 
-      File.open(path, 'wb') do |file|
-        while (data = body.readpartial)
-          file.write(data)
+      if update
+        File.open(path, 'wb') do |file|
+          while (data = body.readpartial)
+            file.write(data)
+          end
         end
-      end
 
-      MiniMagick::Image.new(path).resize(WIDTH)
+        MiniMagick::Image.new(path).resize(WIDTH)
+      end
 
       path.relative_path_from(SOURCE).to_s
     end
